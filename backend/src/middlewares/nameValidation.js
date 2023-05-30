42d21b01ace17cfbef3c67bef0e41737417ec533
hildelio <hildelio@gmail.com>
2023-05-30 19:22:39 -0300

    fix Req 03 :detective:

--- backend/src/middlewares/nameValidation.js
+++ backend/src/middlewares/nameValidation.js
@@ -4,11 +4,11 @@ const nameValidation = (req, res, next) => {
   const { name } = req.body;
   const result = productsSchema.validate({ name });
   const { error } = result;
-  if (error.details[0].type === 'string.min') {
-    return res.status(422).json({ message: error.message });
+  if (!name) {
+    return res.status(400).json({ message: error.message });
   }
   if (error) {
-    return res.status(400).json({ message: error.message });
+    return res.status(422).json({ message: error.message });
   }
   next();
 };
